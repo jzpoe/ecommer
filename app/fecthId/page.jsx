1ad4fde1436b9855dd12dde30e39d0231afc0e95
jzpoe <pmguez1287@gmail.com>
2024-10-20 17:14:32 -0500

    corrrecion en postId 4

--- app/fecthId/page.jsx
+++ app/fecthId/page.jsx
@@ -1,16 +1,16 @@
 async function loadPost(id) {
   try {
-    const res = await fetch(`https://fakestoreapi.com/products/${id}`);
+    const reponse = await fetch(`https://fakestoreapi.com/products/${id}`);
 
-    // Verifica si la respuesta es exitosa (código 200-299)
-    if (!res.ok) {
-      throw new Error(`Error ${res.status}: ${res.statusText}`);
+    // Verifica si la reponsepuesta es exitosa (código 200-299)
+    if (!reponse.ok) {
+      throw new Error(`Error ${reponse.status}: ${reponse.statusText}`);
     }
 
-    // Verifica el tipo de contenido de la respuesta
-    const contentType = res.headers.get("content-type");
+    // Verifica el tipo de contenido de la reponsepuesta
+    const contentType = reponse.headers.get("content-type");
     if (contentType && contentType.includes("application/json")) {
-      const data = await res.json();
+      const data = await reponse.json();
       return data; // Devuelve los datos parseados
     } else {
       throw new Error("Expected JSON but received another content type");
